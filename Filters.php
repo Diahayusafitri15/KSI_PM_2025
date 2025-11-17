f19073b8885e966a47fd688c2cae2c8e9412b40b
Diahayusafitri15 <144880042+Diahayusafitri15@users.noreply.github.com>
2025-11-17 16:49:39 +0700

    Add files via upload

---
+++ Filters.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Config\Filters as BaseFilters;
+use CodeIgniter\Filters\Cors;
+use CodeIgniter\Filters\CSRF;
+use CodeIgniter\Filters\DebugToolbar;
+use CodeIgniter\Filters\ForceHTTPS; // <-- PASTIKAN USE STATEMENT INI ADA
+use CodeIgniter\Filters\Honeypot;
+use CodeIgniter\Filters\InvalidChars;
+use CodeIgniter\Filters\PageCache;
+use CodeIgniter\Filters\PerformanceMetrics;
+use CodeIgniter\Filters\SecureHeaders;
+
+class Filters extends BaseFilters
+{
+    /**
+     * Configures aliases for Filter classes to
+     * make reading things nicer and simpler.
+     *
+     * [filter_name => classname]
+     */
+    public array $aliases = [ 
+        'csrf'          => CSRF::class,
+        'toolbar'       => DebugToolbar::class,
+        'honeypot'      => Honeypot::class,
+        'invalidchars'  => InvalidChars::class,
+        'secureheaders' => SecureHeaders::class,
+        'cors'          => Cors::class,
+        
+        // PERBAIKAN: Alias yang hilang untuk filter bawaan
+        'forcehttps'    => ForceHTTPS::class, 
+        'pagecache'     => PageCache::class,
+        'performance'   => PerformanceMetrics::class,
+        
+        // Filter kustom Anda (AdminFilter)
+        'adminauth'     => \App\Filters\AdminFilter::class, 
+    ];
+
+    /**
+     * List of special required filters.
+     */
+    public array $required = [
+        'before' => [
+            'forcehttps', // Sekarang alias ini dikenali
+            'pagecache',  // Web Page Caching
+        ],
+        'after' => [
+            'pagecache',   // Web Page Caching
+            'performance', // Performance Metrics
+            'toolbar',     // Debug Toolbar
+        ],
+    ];
+
+    /**
+     * List of filter aliases that are always
+     * applied before and after every request.
+     */
+    public array $globals = [
+        'before' => [
+            // 'honeypot',
+            // 'csrf',
+            // 'invalidchars',
+        ],
+        'after' => [
+            // 'honeypot',
+            // 'secureheaders',
+        ],
+    ];
+
+    /**
+     * List of filter aliases that works on a
+     * particular HTTP method (GET, POST, etc.).
+     */
+    public array $methods = [];
+
+    /**
+     * List of filter aliases that should run on any
+     * before or after URI patterns.
+     */
+    public array $filters = [];
+}
